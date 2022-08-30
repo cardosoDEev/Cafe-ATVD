a994555289cd3a51db7730342d9cf74817b9bb41
cardosoDEev <110579843+cardosoDEev@users.noreply.github.com>
2022-08-30 12:17:59 -0300

    Atividade de comentar codigo
    Código feito em sala de aula com auxilio do professor , Usando Bootstrap,Modal ,DJSON

---
+++ LIS-main/cafeteria-app/js/dataset.js
@@ -0,0 +1,31 @@
+//Dataset tratar Dados do banco de dados
+let nomeCafeteria = "DustCoffe";
+//Itens do formulario com as definições de nome, imagem,preço e descrição
+let itens = [
+  {
+    nome: "Xícara de café pequeno",
+    imagem: "./imagem/cafe.jpg",
+    preco: "25.0",
+    descricao: "Café arábica orgânico.",
+  },
+  {
+    nome: "Xícara de café grande",
+    imagem: "./imagem/cafe.jpg",
+    preco: "50.0",
+    descricao: "Café arábica orgânico.",
+  },
+  {
+    nome: "Cookie belga",
+    imagem: "./imagem/cookie.jpg",
+    preco: "25.0",
+    descricao: "Cookie descontruido de chocolate belga.",
+  },
+  {
+    nome: "Bolo express",
+    imagem: "./imagem/bolo.jpg",
+    preco: "25.0",
+    descricao: "Bolo a base de café expresso da casa.",
+  },
+];
+
+export default itens;
